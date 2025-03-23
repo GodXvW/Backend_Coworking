7d9b2796e8e3ab3981739b65445e79051694b976
Siphax <73622634+GodXvW@users.noreply.github.com>
2025-03-23 20:26:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 const app = express();
+app.use(cors());
 // app.use(cors({ origin: "https://coworkings.vercel.app", credentials: true }));
 // require("./function/cronReminder"); // Load the cron job
 
